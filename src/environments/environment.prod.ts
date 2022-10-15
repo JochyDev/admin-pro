d85f2583e0751271a9d0618cd45c9d6aded74a63
JochyDev <joseluisjimenez1998@gmail.com>
2022-10-15 11:02:42 -0400

    [update] enviroment prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'http://localhost:8080/api',
+  base_url: 'https://mellow-cactus-70d967.netlify.app/api',
   GOOGLE_ID:'32294195677-oepq1bjt1beeu42hk2ucsfjhdnca6sla.apps.googleusercontent.com'
 };
