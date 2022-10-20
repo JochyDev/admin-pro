dd620fe15a9d45ac46e3abc33980efa3f1daa635
JochyDev <joseluisjimenez1998@gmail.com>
2022-10-20 17:54:06 -0400

    [update] appComponent tested

--- __test__/app.component.spec.ts
+++ __test__/app.component.spec.ts
@@ -1,31 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../src/app/app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [  
+        RouterTestingModule
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  test('should create the app', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'adminpro'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('adminpro');
+  test(`should have as title 'adminpro'`, () => {
+    expect(component.title).toEqual('adminpro');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('adminpro app is running!');
-  // });
+  test(`should match with snapshot`, () => {
+    expect(compiled).toMatchSnapshot();
+  })
 });
